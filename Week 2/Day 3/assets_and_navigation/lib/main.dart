03be8fc360aada511d8ea1320b5d3e90538e90b7
tommyrachmadiono <tommyrachmadiono@gmail.com>
2020-02-06 16:19:51 +0700

    navigator

--- Week 2/Day 3/assets_and_navigation/lib/main.dart
+++ Week 2/Day 3/assets_and_navigation/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'second.dart';
 
 void main() => runApp(MyApp());
@@ -8,7 +9,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Home Page',
-      home: HomePage(),
+      // home: HomePage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/secondNamed': (context) => SecondNamedPage(),
+        ExtractParameters.routeName: (context) => ExtractParameters(),
+      },
     );
   }
 }
@@ -34,19 +41,51 @@ class HomePage extends StatelessWidget {
                   child: Image.network('https://picsum.photos/250?image=9')),
             ),
             RaisedButton(
-              child: Text('CLICK me!!'),
+              child: Text('Navigate to another routes using Navigator.push'),
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => SecondRoute()),
+                  MaterialPageRoute(builder: (context) => SecondPage()),
                 );
               },
+            ),
+            RaisedButton(
+              child: Text('Navigate with named routes'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/secondNamed');
+              },
+            ),
+            RaisedButton(
+              child: Text('Navigate with parameters to named routes'),
+              onPressed: () {
+                Navigator.pushNamed(context, ExtractParameters.routeName,
+                    arguments: PassParameter('extract arguments',
+                        'this is message extracted in the build method'));
+              },
+            ),
+            Builder(
+              builder: (ctx) => RaisedButton(
+                child: Text('Return data from second screen'),
+                onPressed: () {
+                  _navigateAndDisplayOption(ctx);
+                },
+              ),
+            ),
+            TodoScreen(
+              todos: List.generate(
+                5,
+                (i) => Todo(
+                  'Todo $i',
+                  'A description of what needs to be done for Todo $i',
+                ),
+              ),
             ),
             Image(
               image: AssetImage('assets/logo-putih-kotak.png'),
               fit: BoxFit.contain,
             ),
             Image(
+              height: 150,
               image: NetworkImage(
                   "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRWH48ZCFtZs9dptnr9opMckjzghA4Vxlyn2ZLaAiRnMpNKW9bH"),
               fit: BoxFit.contain,
@@ -55,6 +94,19 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  _navigateAndDisplayOption(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SelectionScreen()),
+    );
+
+    Scaffold.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(SnackBar(
+        content: Text("$result"),
+      ));
   }
 }
 
@@ -79,51 +131,62 @@ class DetailScreen extends StatelessWidget {
   }
 }
 
-class SecondRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("APP bar SECOND ROUTE"),
-          automaticallyImplyLeading: true,
-        ),
-        body: Center(
-          child: RaisedButton(
-            child: Text("BACK TO HOMEPAGE"),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
+class ExtractParameters extends StatelessWidget {
+  static const routeName = '/passParameters';
 
-class SecondRandom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SomePage();
-    
-  }
-}
+    final PassParameter param = ModalRoute.of(context).settings.arguments;
 
-class SomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("APP bar RANDOM"),
+        title: Text(param.title),
       ),
       body: Center(
-        child: RaisedButton(
-          child: Text("BACK TO HOMEPAGE"),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+        child: Text(param.message),
       ),
     );
   }
+}
+
+class TodoScreen extends StatelessWidget {
+  final List<Todo> todos;
+
+  const TodoScreen({Key key, this.todos}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: todos.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(todos[index].title),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailTodo(),
+                settings: RouteSettings(arguments: todos[index]),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class PassParameter {
+  final String title;
+  final String message;
+
+  PassParameter(this.title, this.message);
+}
+
+class Todo {
+  final String title;
+  final String description;
+
+  Todo(this.title, this.description);
 }
--- Week 2/Day 3/assets_and_navigation/lib/second.dart
+++ Week 2/Day 3/assets_and_navigation/lib/second.dart
@@ -1,15 +1,9 @@
+import 'package:assets_and_navigation/main.dart';
 import 'package:flutter/material.dart';
 
 // void main() => runApp(SecondPage());
 
 class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SomePage();
-  }
-}
-
-class SomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +12,82 @@ class SomePage extends StatelessWidget {
       ),
       body: Center(
         child: RaisedButton(
-          child: Text("BACK TO HOMEPAGE"),
+          child: Text("BACK TO FIRST ROUTE using Navigator.pop"),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
+      ),
+    );
+  }
+}
+
+class SecondNamedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Named Page'),
+      ),
+      body: Center(
+        child: RaisedButton(
+            child: Text('BACK TO FIRST NAMED ROUTE'),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+      ),
+    );
+  }
+}
+
+class SelectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pick any'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.pop(context, 'Yep!');
+                },
+                child: Text('Yep!'),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.pop(context, 'Yoink!');
+                },
+                child: Text('Yoink!'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailTodo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Todo todo = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Text(todo.description),
       ),
     );
   }
